561db4fa45a32ad15544b2da257f955dd618cfa9
yash garala <yashgarala29@gmail.com>
2020-10-13 09:51:29 +0530

    AT project

--- ANGULAR/AT-social-media-project/src/app/app.module.ts
+++ ANGULAR/AT-social-media-project/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-
+import { HomePageComponent } from './home-page/home-page.component';
+import{MatIconModule} from '@angular/material/icon'
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule
   ],
+  exports:[MatIconModule],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- ANGULAR/AT-social-media-project/src/app/navigation-bar/navigation-bar.component.ts
+++ ANGULAR/AT-social-media-project/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{MatIcon} from '@angular/material/icon'
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
