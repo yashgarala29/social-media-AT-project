f311ae62ec98109e70d9e36154a41f5ee24adba8
yash garala <yashgarala29@gmail.com>
2020-10-16 11:30:15 +0530

    AT project

--- ANGULAR/AT-social-media-project/src/index.js
+++ ANGULAR/AT-social-media-project/src/index.js
@@ -4,12 +4,20 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bearerToken = require('express-bearer-token');
 const oktaAuth = require('./auth');
-const hangman = require('./');
+const social_media_db = require('./social_media_db');
+const app = express()
 const port = process.env.PORT
+
 const app = express()
   .use(cors())
   .use(bodyParser.json())
   .use(bearerToken())
   .use(oktaAuth)
-  .use(hangman());
-  
+  .use(social_media_db());
+mongoose.connect(`mongodb://localhost:27017/social_media_db`)
+.then(() => {
+  console.log('Connected to database');
+  app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+  });
+});
